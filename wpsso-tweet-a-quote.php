094ef8b5a70ed2840283152b0a968bb226353af8
JS Morisset <jsm@surniaulula.com>
2017-04-20 20:12:44 +0200

    v1.1.8-rc.1

--- wpsso-tweet-a-quote.php
+++ wpsso-tweet-a-quote.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to add CSS Twitter-style quoted text with a Tweet share link to post and page content (uses easily customized CSS).
  * Requires At Least: 3.7
- * Tested Up To: 4.7.3
+ * Tested Up To: 4.7.4
  * Version: 1.1.8-rc.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
