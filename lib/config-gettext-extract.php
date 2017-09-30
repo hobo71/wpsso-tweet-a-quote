c708d0f70e5bf5c5a70382e919d705307f5d7cbb
JS Morisset <jsm@surniaulula.com>
2017-09-30 11:09:51 -0400

    v1.1.10

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array(s)
+// extract of translatable text strings from static config array
 _x( 'Tweet a Quote', 'lib file description', 'wpsso-tweet-a-quote' );
 _x( 'WPSSO extension to add CSS Twitter-style quoted text with a Tweet share link to post and page content (uses easily customized CSS).', 'plugin description', 'wpsso-tweet-a-quote' );
 ?>
