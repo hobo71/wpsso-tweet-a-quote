894ae28bae2a2c896fabcbb9f703513738d1a601
JS Morisset <jsm@surniaulula.com>
2018-05-10 15:53:47 -0400

    v2.0.0-dev.1

--- lib/config.php
+++ lib/config.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoTaqConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
-						'min_version' => '4.3.0-rc.3',
+						'min_version' => '4.3.0',
 					),
 					'img' => array(
 						'icons' => array(
