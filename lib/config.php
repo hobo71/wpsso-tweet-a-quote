7316f1c5f14c45762ffd3ed26a56542a2c11f2b9
JS Morisset <jsm@surniaulula.com>
2018-06-24 13:39:37 -0600

    v2.0.0-dev.0

--- lib/config.php
+++ lib/config.php
@@ -73,7 +73,7 @@ if ( ! class_exists( 'WpssoTaqConfig' ) ) {
 
 		public static function set_constants( $plugin_filepath ) { 
 
-			if ( defined( 'WPSSOTAQ_VERSION' ) ) {			// execute and define constants only once
+			if ( defined( 'WPSSOTAQ_VERSION' ) ) {	// Define constants only once.
 				return;
 			}
 
--- lib/register.php
+++ lib/register.php
@@ -76,7 +76,9 @@ if ( ! class_exists( 'WpssoTaqRegister' ) ) {
 		}
 
 		private function activate_plugin() {
+
 			$version = WpssoTaqConfig::$cf['plugin']['wpssotaq']['version'];	// only our config
+
 			if ( class_exists( 'WpssoUtil' ) ) {
 				WpssoUtil::save_all_times( 'wpssotaq', $version );
 			} else {
