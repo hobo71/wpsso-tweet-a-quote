2fb115de0a0afa55bd091b538f1ed8a3b1aa3d7b
JS Morisset <jsm@surniaulula.com>
2018-07-22 15:34:07 -0600

    v1.4.0

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoTaqConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssotaq' => array(			// Plugin acronym.
-					'version'     => '1.4.0-rc.2',	// Plugin version.
+					'version'     => '1.4.0',	// Plugin version.
 					'opt_version' => '10',		// Increment when changing default option values.
 					'short'       => 'WPSSO TAQ',	// Short plugin name.
 					'name'        => 'WPSSO Tweet a Quote',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoTaqConfig' ) ) {
 					'req' => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '4.9.0-rc.2',
+						'min_version' => '4.9.0',
 					),
 					'img' => array(
 						'icons' => array(
