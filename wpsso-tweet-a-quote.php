06e49e5472b1d608c0ede7d6c1b8ab7c7ae415e1
JS Morisset <jsm@surniaulula.com>
2019-04-09 16:50:25 -0600

    v1.4.3

--- wpsso-tweet-a-quote.php
+++ wpsso-tweet-a-quote.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on offers Twitter-style quoted text for your content, with a convenient Tweet share link and customizable CSS.
  * Requires At Least: 3.8
  * Tested Up To: 5.1.1
- * Version: 1.4.2
+ * Version: 1.4.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
